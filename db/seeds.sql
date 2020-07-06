63368ee460b090f9cf5d80187e1e1d060e91bcda
Jorge Guzman <jorguzman100@gmail.com>
2020-07-06 17:10:53 -0500

    DB Setup ready

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Beef');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Big');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Small');
+INSERT INTO burgers (burger_name) VALUES ('Vegan');
+INSERT INTO burgers (burger_name) VALUES ('Chicken');
+INSERT INTO burgers (burger_name) VALUES ('Beef');
+INSERT INTO burgers (burger_name) VALUES ('Big');
+INSERT INTO burgers (burger_name) VALUES ('Small');

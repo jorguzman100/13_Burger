be84f2bbb99c76b76cc2024104be3bfd46b6ea2a
Jorge Guzman <jorguzman100@gmail.com>
2020-07-06 16:12:07 -0500

    Seeds created

--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,7 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
-    id AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beef');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Small');
